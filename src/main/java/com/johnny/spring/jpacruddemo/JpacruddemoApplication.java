788247a1c227ad9fef9d4d2712d62b67d064fbc8
johnny <john.robertson88@yahoo.com>
2024-05-07 19:43:16 -0700

    Initial commit

---
+++ src/main/java/com/johnny/spring/jpacruddemo/JpacruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.johnny.spring.jpacruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpacruddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnny/spring/jpacruddemo/JpacruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnny.spring.jpacruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpacruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
