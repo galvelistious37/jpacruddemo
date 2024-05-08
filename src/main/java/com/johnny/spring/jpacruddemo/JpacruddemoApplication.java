25c8b80a45b256d93eb6b04f11e955dd16db54db
johnny <john.robertson88@yahoo.com>
2024-05-07 20:39:08 -0700

    Set the command line runner object to generate an Instructor and InstructorDetail objects and bind them by setting the InstructorDetail field of the Instructor object

--- src/main/java/com/johnny/spring/jpacruddemo/JpacruddemoApplication.java
+++ src/main/java/com/johnny/spring/jpacruddemo/JpacruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.johnny.spring.jpacruddemo;
 
+import com.johnny.spring.jpacruddemo.dao.AppDAO;
+import com.johnny.spring.jpacruddemo.entity.Instructor;
+import com.johnny.spring.jpacruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,32 @@ public class JpacruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+		// Create the instructor
+		Instructor tempInstructor =
+				new Instructor(
+						"John",
+						"Robertson",
+						"john@email.com");
+
+		// Create the InstructorDetail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail(
+						"http://someurl",
+						"gee-tar");
+
+		// Associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// Save the Instructor
+		System.out.println("Saving: " + tempInstructor);
+		appDAO.save(tempInstructor);
 	}
 
 }
